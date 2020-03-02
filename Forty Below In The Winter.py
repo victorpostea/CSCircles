27c2e18688c71a652c4fe5c57cce364f59d145de
victorpostea <60620574+victorpostea@users.noreply.github.com>
2020-03-02 13:35:58 -0500

    Update Forty Below In The Winter.py
    I managed to do it like this instead of using lists just thought it would be easier and I'm not too sure how to leave a comment so I hope this works. (Just a different way to do it)

--- Forty Below In The Winter.py
+++ Forty Below In The Winter.py
@@ -1,35 +1,11 @@
-temp = input()
-list = []
-tempList = []
-if temp.endswith("F" or "f"):
-    for i in range(0, len(temp)):
-         if temp[i] == 'F':
-             list.append(temp[i])
-         elif temp[i] == 'f':
-             list.append(temp[i])
-         elif temp[i] == '-':
-             list.append(temp[i])
-         elif temp[i] != '-':
-             tempList.append(temp[i])       
-    f = ''.join(tempList)
-    if list[0] == '-':
-        c = (float(f) * (-1) - 32) * (5 / 9)
-    else:
-        c = (float(f) - 32) * (5 / 9)
-    print(str(c)+'C')
-elif temp.endswith("C" or "c"):
-    for i in range(0, len(temp)):
-         if temp[i] == 'C':
-             list.append(temp[i])
-         elif temp[i] == 'c':
-             list.append(temp[i])
-         elif temp[i] == '-':
-             list.append(temp[i])
-         elif temp[i] != '-':
-             tempList.append(temp[i])       
-    c = ''.join(tempList)
-    if list[0] == '-':
-        f = (float(c) * (-1)) * (9 / 5) + 32
-    else:
-        f = float(c) * (9 / 5) + 32
-    print(str(f)+'F')
+x = input()
+if x[len(x) - 1] == 'C':
+   c = x[0:len(x) - 1]
+   c1 = float(c)
+   f = (c1 * 9) / 5 + 32
+   print(str(f) + 'F')
+elif x[len(x) - 1] == 'F':
+   f = x[0:len(x) - 1]
+   f1 = float(f)
+   c = ((f1-32) * 5) / 9
+   print(str(c) + 'C')
